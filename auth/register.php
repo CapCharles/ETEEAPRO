e0ea7f501572808657211c038cc612524974e3fd
CapCharles <pobletecharles11@gmail.com>
2025-10-02 17:39:18 +0800

    back

--- auth/register.php
+++ auth/register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once './config/database.php';
+require_once '../config/database.php';
 require_once '../config/constants.php';
 include_once '../includes/email_notifications.php';
 

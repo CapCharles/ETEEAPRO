b42d1f5ef28de69e6f2a71e3fbb62e95502779d8
CapCharles <pobletecharles11@gmail.com>
2025-10-02 18:17:00 +0800

    try

--- includes/email_notifications.php
+++ includes/email_notifications.php
@@ -5,9 +5,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'C:\xampp\PHPMailer\PHPMailer\src\Exception.php';
-require 'C:\xampp\PHPMailer\PHPMailer\src\PHPMailer.php';
-require 'C:\xampp\PHPMailer\PHPMailer\src\SMTP.php';
+require 'PHPMailer\PHPMailer\src\Exception.php';
+require 'PHPMailer\PHPMailer\src\PHPMailer.php';
+require 'PHPMailer\PHPMailer\src\SMTP.php';
 
 function makeMailer(): PHPMailer {
     $mail = new PHPMailer(true);
